290837c21cdeac74ad220beab7722d9fe279d53a
alamin40 <hossain.78295@gmail.com>
2022-02-01 13:01:54 +0600

    up

--- app/classes/Calculator.php
+++ app/classes/Calculator.php
@@ -3,27 +3,68 @@ namespace App\classes;
 use App\classes\User;
 class Calculator extends User
 {
-    public $firstNumber = 10;
+    public $firstNumber;
     public $secondNumber;
     public $operator;
     public $result;
     public $user;
 
-    public function __construct()
+    public function __construct($data)
     {
-        echo 'In construct';
+        $this->firstNumber  = $data['first_number'];
+        $this->secondNumber = $data['second_number'];
+        $this->operator     = $data['operator'];
+
+
+//        echo $data[2];
+//        print_r($data);
+
+//        echo is_array($data) ? 'data is array ' : 'data is single variable';
     }
 
     public function index(){
-//        $this->user = new User();
-        echo $this->logout();
+        switch ($this->operator)
+        {
+            case '+':
+                $this->result = $this->add();
+                break;
+
+            case '-':
+                $this->result = $this->sub();
+                break;
+
+            case '*':
+                $this->result = $this->multiplication();
+                break;
+
+//            case '/':
+//                echo $this->de();
+//                break;
+//
+//            case '%':
+//                echo $this->multiplication();
+//                break;
+        }
+
+        return $this->result;
+
+
     }
 
-    public function one(){
-        echo 'In one';
+
+
+    protected function add()
+    {
+        return $this->firstNumber + $this->secondNumber;
+
     }
 
-    private function two(){
-        echo 'In two';
+    protected function sub()
+    {
+        return $this->firstNumber - $this->secondNumber;
+    }
+    protected function multiplication()
+    {
+        return $this->firstNumber * $this->secondNumber;
     }
 }
